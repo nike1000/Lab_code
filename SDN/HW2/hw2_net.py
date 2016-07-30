b3a079142eee55e8c92fda3b8e5cb49d78010eed
nike1000 <st505102000@gmail.com>
2016-07-31 00:43:36 +0800

    wrong use hw1_net.py as hw2_net.py, modify it

--- SDN/HW2/hw2_net.py
+++ SDN/HW2/hw2_net.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python2
 
-"""
-2016.07.29,second homework from lab
-2 host 1 switch with secure failmode
-remote controller with Openflow protocol
-
-$ sudo python hw2_net.py or sudo ./hw2_net.py
-"""
-
-
 from mininet.log import setLogLevel, info
 from mininet.net import Mininet
 from mininet.cli import CLI
+from mininet.node import RemoteController, OVSSwitch
 
 def MininetTopo():
     net = Mininet()
@@ -21,19 +13,19 @@ def MininetTopo():
     righthost = net.addHost("h2")
     
     info("Create switch node.\n")
-    #switch = net.addSwitch("s1", switch=OVSSwitch, protocols = 'OpenFlow15', failMode = 'standalone')
     switch = net.addSwitch("s1", switch=OVSSwitch, protocols = 'OpenFlow15', failMode = 'secure')
+    #switch = net.addSwitch("s1", switch=OVSSwitch, protocols = 'OpenFlow15', failMode = 'standalone')
     
-    info("Create Links.\n")
-    # let host connect to switch port 3 and 4
-    net.addLink(lefthost, switch, port2=3)
-    net.addLink(righthost, switch, port2=4)
+    info("Connect to controller node.\n")
+    net.addController(name='c1',controller=RemoteController,ip='192.168.1.22',port=6633)
 
-    info("Build and start network.\n")
+    info("Create Links.\n")
+    net.addLink(lefthost, switch)
+    net.addLink(righthost, switch)
+
+    info("build and start.\n")
     net.build()
     net.start()
-    
-    info("Run mininet CLI.\n")
     CLI(net)
 
 
