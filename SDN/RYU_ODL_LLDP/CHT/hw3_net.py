6e7caaead4fc236e01d864c889f1533b9acf8e8a
nike1000 <st505102000@gmail.com>
2017-02-28 22:52:44 +0800

    Add directory CHT to store the program of CHT project

---
+++ SDN/RYU_ODL_LLDP/CHT/hw3_net.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+"""
+2016.07.31
+2 host n switch, topo is link, with failmode setting
+
+h1--s1--...--sn--h2
+
+$ sudo python hw3_net.py [switchnum] or sudo ./hw3_net.py [switchnum]
+"""
+
+import sys
+from mininet.log import setLogLevel, info
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.node import RemoteController, OVSSwitch
+
+def MininetTopo(switchnum):
+    # 存放 switch 參照
+    switchlist = []
+
+    # 產生一個 Mininet Object
+    net = Mininet()
+
+    # 在 Mininet 中加入兩個 hosts
+    info("Create host nodes.\n")
+    lefthost = net.addHost("h1")
+    righthost = net.addHost("h2")
+    
+    info("Create switch node.\n")
+    
+    # 連接至 remote controller, 6633 為 Ryu controller 預設 port
+    info("Connect to controller node.\n")
+    c1 = net.addController(name='c1',controller=RemoteController,ip='192.168.89.129',port=6633)
+    c2 = net.addController(name='c2',controller=RemoteController,ip='192.168.89.130',port=6634)
+
+    count = 1
+    while count <= int(switchnum):
+        # switch name 設為 s1, s2, s3...
+        switchname = "s" + str(count)
+        # 加入新的 switch, switch 種類使用 OVSSwitch (即 OpenvSwitch) 取代預設的 Linux OVSKernelSwitch, OpenFlow protocol 使用 1.3 版, 最後將參照存放進 list
+        switchlist.append(net.addSwitch(switchname, switch=OVSSwitch, protocols='OpenFlow13', failMode='secure'))
+        count+=1
+    
+
+    # 加入 link, 串起 h1 和 s1
+    info("Create Links.\n")
+    net.addLink(lefthost, switchlist[0])
+    
+    # s2 之後每台 switch 和前一台連接
+    count=1
+    while count <= int(switchnum)-1:
+        net.addLink(switchlist[count-1],switchlist[count])
+        count+=1
+
+    # 加入 link, 串起 sn 和 h2
+    net.addLink(righthost, switchlist[len(switchlist)-1])
+
+    info("build and start.\n")
+    # 建立 topo
+    net.build()
+    c1.start()
+    c2.start()
+    
+    switchlist[0].start([c1])
+    switchlist[1].start([c1])
+    switchlist[2].start([c1])
+    switchlist[3].start([c2])
+    switchlist[4].start([c2])
+    switchlist[5].start([c2])
+    
+
+
+    # 啟動 switches 和 controller
+    #net.start()
+    # 進入 Command Line Interface
+    CLI(net)
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+
+    # 取得參數為建立 switch 數量, 預設為 1
+    if len(sys.argv) > 2:
+        print "Too much argv!!"
+        sys.exit(0)
+    elif len(sys.argv) == 2:    
+        MininetTopo(sys.argv[1])
+    else:
+        MininetTopo(1)
+
---
+++ SDN/RYU_ODL_LLDP/CHT/hw3_ryuapp.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# 2016.07.31 kshuang
+
+from ryu.base import app_manager
+from ryu.ofproto import ofproto_v1_3
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller import ofp_event
+from ryu.ofproto import ofproto_v1_3_parser
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import lldp
+from ryu.lib.packet import packet 
+from ryu import utils
+import struct
+
+class MyRyu(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    normal_port = []
+    lldp_topo = {}
+
+    def __init__(self, *args, **kwargs):
+        super(MyRyu, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        self.send_port_stats_request(datapath)
+
+
+    def send_port_stats_request(self, datapath):
+        ofp = datapath.ofproto
+        ofp_parser = datapath.ofproto_parser
+        req = ofp_parser.OFPPortDescStatsRequest(datapath, 0, ofp.OFPP_ANY)
+        datapath.send_msg(req)
+
+
+    @set_ev_cls(ofp_event.EventOFPPortDescStatsReply, MAIN_DISPATCHER)
+    def port_stats_reply_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # LLDP packet to controller
+        match = parser.OFPMatch(eth_type=ether_types.ETH_TYPE_LLDP)
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
+        self.add_flow(datapath, 0, match, actions)
+        
+        for stat in ev.msg.body:
+            if stat.port_no < ofproto.OFPP_MAX:
+                self.normal_port.append(stat.port_no)
+                self.send_lldp_packet(datapath, stat.port_no, stat.hw_addr)
+
+        if len(self.normal_port) == 2:
+            # port B to port A
+            match = parser.OFPMatch(in_port=self.normal_port[0])
+            actions = [parser.OFPActionOutput(self.normal_port[1])]
+            self.add_flow(datapath, 0, match, actions)
+
+            # port A to port B
+            match = parser.OFPMatch(in_port=self.normal_port[1])
+            actions = [parser.OFPActionOutput(self.normal_port[0])]
+            self.add_flow(datapath, 0, match, actions)
+
+        # clear port record after add flow entry
+        self.normal_port = []
+
+    def add_flow(self, datapath, priority, match, actions):
+        ofp = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofp.OFPIT_APPLY_ACTIONS,actions)]
+
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, command=ofp.OFPFC_ADD, match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+
+    def send_lldp_packet(self, datapath, port_no, hw_addr):
+        ofp = datapath.ofproto
+        pkt = packet.Packet()
+        pkt.add_protocol(ethernet.ethernet(ethertype=ether_types.ETH_TYPE_LLDP,src=hw_addr ,dst=lldp.LLDP_MAC_NEAREST_BRIDGE))
+        
+        prefix='\x00\x00'
+        tlv_chassis_id = lldp.ChassisID(subtype=4, chassis_id=prefix+str(struct.pack(">I", datapath.id)))
+        tlv_port_id = lldp.PortID(subtype=lldp.PortID.SUB_LOCALLY_ASSIGNED, port_id=str(port_no))
+        tlv_ttl = lldp.TTL(ttl=10)
+        tlv_end = lldp.End()
+        tlvs = (tlv_chassis_id, tlv_port_id, tlv_ttl, tlv_end)
+        pkt.add_protocol(lldp.lldp(tlvs))
+        pkt.serialize()
+
+        data = pkt.data
+        parser = datapath.ofproto_parser
+        actions = [parser.OFPActionOutput(port=port_no)]
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofp.OFP_NO_BUFFER, in_port=ofp.OFPP_CONTROLLER, actions=actions, data=data)
+        datapath.send_msg(out)
+
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        port = msg.match['in_port']
+        pkt = packet.Packet(data=msg.data)
+        
+        pkt_ethernet = pkt.get_protocol(ethernet.ethernet)
+        if not pkt_ethernet:
+            return
+        
+        pkt_lldp = pkt.get_protocol(lldp.lldp)
+        if pkt_lldp:
+            self.handle_lldp(datapath, port, pkt_ethernet, pkt_lldp)
+
+
+    def handle_lldp(self, datapath, port, pkt_ethernet, pkt_lldp):
+        prefix='\x00\x00'
+        self.lldp_topo.setdefault(prefix+str(struct.pack(">I", datapath.id)),{})
+        port_connect = {}
+        self.lldp_topo[prefix+str(struct.pack(">I", datapath.id))].setdefault(port, [pkt_lldp.tlvs[0].chassis_id, pkt_lldp.tlvs[1].port_id])
+        print "==============> "
+        print datapath.id
+        print self.lldp_topo.get(prefix+str(struct.pack(">I", datapath.id)), {})
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg,MAIN_DISPATCHER)
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        self.logger.debug('OFPErrorMsg received: type=0x%02x code=0x%02x message=%s',msg.type, msg.code, utils.hex_array(msg.data))
+
