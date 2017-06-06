abb1bda257fd874acc60ba14a85c27a1c807e34e
nike1000 <st505102000@gmail.com>
2017-06-06 16:43:40 +0800

    Performance Tool Script: using top instead of ps

--- SDN/THESIS/CPU_MEM.sh
+++ SDN/THESIS/CPU_MEM.sh
@@ -4,5 +4,7 @@ pid=`pgrep ryu-manager`
 
 while sleep 1; do
     echo -n `date +%H:%M:%S`
-    ps --no-headers -o '%cpu,%mem' -p $pid;
+    echo -n " "
+    #ps --no-headers -o '%cpu,%mem' -p $pid;
+    top -b -p $pid -n 1 | grep ryu-manager | sed 's/  */ /g' | awk '{print $9, $10}'
 done
